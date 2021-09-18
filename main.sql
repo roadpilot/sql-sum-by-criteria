0dc965136a6fefc0377c41ba15a2171d14e44b37
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-18 19:15:29 +0000

    single line

--- main.sql
+++ main.sql
@@ -1,4 +1,4 @@
 /*
 Query the total population of all cities in CITY where District is California.
 */
-select sum(POPULATION) from CITY where DISTRICT='California'
+select sum(POPULATION) from CITY where DISTRICT='California';
